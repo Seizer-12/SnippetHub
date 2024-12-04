30ebc206bad4666010057943d43d1df9b9c4005a
Seizer <idowuenoch65@gmail.com>
2024-12-04 01:58:19 +0100

    Added styled components for styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Header = () => {
+	return (
+		<header>
+			<nav className="header_nav">
+				<div className="logo">
+					<img
+						src="../components/assets/sniph_logo.webp"
+						alt="Logo"
+						className="Logo_img"
+					></img>
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
---
+++ src/components/styles.js
@@ -0,0 +1,6 @@
+//import React from "react";
+import styled from "styled-components";
+
+export const Header_container = styled.div`
+	background-color: #918119;
+`;
