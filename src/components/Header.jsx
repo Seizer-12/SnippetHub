40da9d1b8b78508bbac83734adb45439c4b82455
Seizer <idowuenoch65@gmail.com>
2024-12-04 03:10:31 +0100

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Header_container, Header_logo, Header_text } from "./styles";
+import sniph_logo from "../assets/sniph_logo.webp";
 
 const Header = () => {
 	return (
-		<header>
-			<nav className="header_nav">
-				<div className="logo">
-					<img
-						src="../components/assets/sniph_logo.webp"
-						alt="Logo"
-						className="Logo_img"
-					></img>
-				</div>
-			</nav>
-		</header>
+		<>
+			<Header_container>
+				<Header_logo src={sniph_logo} alt="Logo" />
+				<Header_text>SnippetHub</Header_text>
+			</Header_container>
+		</>
 	);
 };
 
--- src/components/styles.js
+++ src/components/styles.js
@@ -1,6 +1,31 @@
 //import React from "react";
 import styled from "styled-components";
 
+export const Container = styled.div`
+	background-color: #ffffff;
+	margin: 0 auto;
+	padding: 5px;
+`;
+
 export const Header_container = styled.div`
 	background-color: #918119;
+	display: flex;
+	height: 50px;
+	width: 100%;
+	border-radius: 10px;
+	padding: 5px;
+`;
+
+export const Header_logo = styled.img`
+	border-radius: 50%;
+	height: 50px;
+	width: 50px;
+`;
+
+export const Header_text = styled.p`
+	font-weight: 500;
+	font-size: 30px;
+	font-family: courier;
+	color: #ffffff;
+	margin: auto 10px;
 `;
